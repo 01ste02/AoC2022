26527f0384cca777321bf2e934fcfcd578cb4629
Oskar Stenberg <01ste02@gmail.com>
2022-12-05 09:19:16 +0100

    Day5: Advent of Bash is getting hard. This problem could have been solved MUCH quicker by any list class, but the lists in bash lack methods for operations on elements. So I basically made it. First with sed - which was buggy since crates were not unique and sed removed all crates with a specific insignia. Then I made it with bash comprehensions - MUCH faster and easier to understand. Also solves the bugs with sed.

---
+++ day5/move_stacks.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+stack=()
+declare -A stack_columns
+cols=""
+
+if [ "$#" -gt 0 ]; then
+    printf '%s\n' "$@"
+fi
+while IFS= read -r string; do
+    if [[ "$string" == *"["* ]]; then
+        #We are still in stack definition
+        
+	line=""
+	while [[ ${#string} -gt 2 ]]; do
+	    res="${string:0:4}"
+            string="${string:4}"
+
+	    if [[ "$res" == "   "* ]]; then
+                line+="[_] "
+	    else
+	        line+="$res"
+	    fi
+	done
+        
+	echo "$line"
+	stack+=( "$line" )
+    elif [[ "$string" == *"move"* ]]; then
+        #We are on move instructions
+        echo "Move instructions"
+	num="$(sed -E 's/move ([[:digit:]]*) from .*/\1/g' <<< $string)"
+        fcol="$(sed -E 's/.* from ([[:digit:]]*) to .*/\1/g' <<< $string)"
+	tcol="$(sed -E 's/.* to ([[:digit:]]*).*/\1/g' <<< $string)"
+	echo "Number of boxes to move: $num"
+	echo "From col: $fcol"
+	echo "To col: $tcol"
+        
+	for i in $(seq 1 $num); do
+	    while [[ "${stack_columns[$fcol]:0:4}" == "[_]"* ]]; do
+	        stack_columns[$fcol]="${stack_columns[$fcol]:4}"
+            done
+            crate="${stack_columns[$fcol]:0:3}"
+
+            echo "      Crate: $crate"
+            echo "      From column: ${stack_columns[$fcol]}"
+            echo "      To column: ${stack_columns[$tcol]}"
+
+
+            stack_columns[$fcol]="${stack_columns[$fcol]:4}"
+
+	    while [[ "${stack_columns[$tcol]:0:4}" == "[_]"* ]]; do
+                stack_columns[$tcol]="${stack_columns[$tcol]:4}"
+            done
+            
+	    stack_columns[$tcol]="$crate ${stack_columns[$tcol]}"
+	    echo "	Result to: ${stack_columns[$tcol]}"
+             
+	    echo "	Result from: ${stack_columns[$fcol]}"
+	    #if [[ "${stack_columns[$tcol]}" == *[_]* ]]; then
+	#	tmpRes="$(sed "s/\(.*\)\[\_\]\(.*\)/\1$crate\2/g" <<< ${stack_columns[$tcol]})"
+	 #       echo "	Result to: $tmpRes"
+	  #      stack_columns[$tcol]="$tmpRes"
+	   # else
+            #    echo "	Result to: $crate ${stack_columns[$tcol]}"
+	  #      stack_columns[$tcol]="$crate ${stack_columns[$tcol]}"
+	   # fi
+	 #   tmpRes="$(sed "s/\(.*\)$crate\(.*\)/\1\_\2/g" <<< ${stack_columns[$fcol]})"
+	  #  echo "	Result from: $tmpRes"
+	  #  stack_columns[$fcol]="$tmpRes"
+	    echo "MOVE DONE"
+	done
+    elif [ ! -z "$string" ]; then
+        #We are on the column definition row
+	stack_columns[0]=0
+	cols="$string"
+	for col in $string; do
+	    echo "$col"
+	    for row in "${stack[@]}"; do
+                echo "Row: $row"
+                IFS=" " read -ra elem <<< "${row}"
+		#echo "Element $col: ${elem[1]} ${elem[$(($col-1))]}"
+		stack_columns[$col]+="${elem[$(($col-1))]} "
+            done
+	done
+	echo "Column 9: ${stack_columns[8]}"
+    else
+        #Strange empty row..
+	echo "Empty row in input"
+    fi
+done
+
+
+topString=""
+for i in $(rev <<< $cols); do
+    echo "${stack_columns[$i]}"
+    for item in ${stack_columns[$i]}; do
+        if [[ ! "$item" == "[_]" ]]; then
+	    topString+="$(sed 's/\[\(.*\)\]/\1/g' <<< $item)"
+	    break
+	fi
+    done
+done
+
+topString="$(rev <<< $topString)"
+echo "$topString"
